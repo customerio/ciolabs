96c8727cd221d64bcddca467091a5bd45edb9bd2
Avi Goldman <avrahamymgoldman@gmail.com>
2025-09-16 22:21:47 -0400

    clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,4 @@ module.exports = {
   root: true,
   extends: ['@ciolabs/eslint-config/node'],
   ignorePatterns: ['node_modules/', 'dist/', 'packages/*/node_modules/', 'packages/*/dist/'],
-  overrides: [
-    {
-      files: ['packages/**/*.js'],
-      rules: {
-        'no-console': 'off',
-      },
-    },
-  ],
 };
