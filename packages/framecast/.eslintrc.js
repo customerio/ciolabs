b104255e82b5f93454de34ffa36391c29e441ec0
Avi Goldman <avrahamymgoldman@gmail.com>
2025-09-30 10:37:22 -0400

    migrate from framecast to @ciolabs/framecast

---
+++ packages/framecast/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    'unicorn/consistent-function-scoping': 'off',
+    'unicorn/no-invalid-remove-event-listener': 'off',
+    'import/no-named-as-default-member': 'off',
+  },
+};
---
+++ packages/framecast/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup';
+
+import baseConfig from '../../tsup.config';
+
+export default defineConfig({
+  ...baseConfig,
+  entry: ['src/index.ts'],
+  noExternal: ['nanostores'],
+});
