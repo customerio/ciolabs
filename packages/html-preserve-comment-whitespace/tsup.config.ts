0c3fb4697c9fb5a6e87bce07df02ccc221f4827c
Avi Goldman <avrahamymgoldman@gmail.com>
2026-02-19 12:47:28 -0800

    Bundle ranges-apply and its deps (ESM-only) into CJS dist

--- packages/html-preserve-comment-whitespace/tsup.config.ts
+++ packages/html-preserve-comment-whitespace/tsup.config.ts
@@ -5,4 +5,5 @@ import baseConfig from '../../tsup.config';
 export default defineConfig({
   ...baseConfig,
   entry: ['src/index.ts'],
+  noExternal: ['ranges-apply', 'ranges-merge', 'tiny-invariant'],
 });
